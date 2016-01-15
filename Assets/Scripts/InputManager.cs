3919226ca89736b50881c7936093da9849b527dd
yuo123 <aaa444777@gmail.com>
2016-01-15 19:26:39 +0200

    Implemented body part selection, and changed tags

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,12 +7,14 @@ public class InputManager : MonoBehaviour
 {
     public Vector2[] pos;
     public Dictionary<string, Vector2> poses;
-    public GameObject markGO;
+    public GameObject brainMarkGO;
+    public GameObject bodyMarkGO;
 
     // Use this for initialization
     void Start()
     {
         poses = new Dictionary<string, Vector2>(pos.Length);
+        //brain parts
         poses.Add("Frontal", pos[0]);
         poses.Add("Hypophysis", pos[1]);
         poses.Add("Thalamus", pos[2]);
@@ -21,6 +23,12 @@ public class InputManager : MonoBehaviour
         poses.Add("Stem", pos[5]);
         poses.Add("Occipital", pos[6]);
         poses.Add("Parietal", pos[7]);
+        //body parts
+        poses.Add("Lungs", pos[8]);
+        poses.Add("Heart", pos[9]);
+        poses.Add("Liver", pos[10]);
+        poses.Add("Legs", pos[11]);
+        poses.Add("Arms", pos[12]);
     }
 
     // Update is called once per frame
@@ -31,9 +39,10 @@ public class InputManager : MonoBehaviour
             Vector3 camPos = Camera.main.transform.position;
             RaycastHit2D hitInfo  = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero, 0f);
             bool res = hitInfo.collider != null;
-            if (res && poses.ContainsKey(hitInfo.collider.tag))
+            if (res && poses.ContainsKey(hitInfo.collider.name))
             {
-                markGO.transform.position = poses[hitInfo.collider.tag];
+                GameObject mark = hitInfo.collider.tag == "BrainPart" ? brainMarkGO : bodyMarkGO;
+                mark.transform.position = poses[hitInfo.collider.name];
             }
         }
     }
