3aab9fb199dce469bd1169f3f2a1c4ac493d3be9
yuo123 <aaa444777@gmail.com>
2016-01-22 19:51:05 +0200

    Changed the way marks work to be based on game objects only

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,31 +5,14 @@ using System.Linq;
 
 public class InputManager : MonoBehaviour
 {
-    public Vector2[] pos;
-    public Dictionary<string, Vector2> poses;
     public GameObject brainMarkGO;
     public GameObject bodyMarkGO;
     public GameObject selectedBrainPart;
+    public GameObject selectedBodyPart;
 
     // Use this for initialization
     void Start()
     {
-        poses = new Dictionary<string, Vector2>(pos.Length);
-        //brain parts
-        poses.Add("Frontal", pos[0]);
-        poses.Add("Hypophysis", pos[1]);
-        poses.Add("Thalamus", pos[2]);
-        poses.Add("Limbic", pos[3]);
-        poses.Add("Cerebellum", pos[4]);
-        poses.Add("Stem", pos[5]);
-        poses.Add("Occipital", pos[6]);
-        poses.Add("Parietal", pos[7]);
-        //body parts
-        poses.Add("Lungs", pos[8]);
-        poses.Add("Heart", pos[9]);
-        poses.Add("Liver", pos[10]);
-        poses.Add("Legs", pos[11]);
-        poses.Add("Arms", pos[12]);
         
     }
 
@@ -40,16 +23,24 @@ public class InputManager : MonoBehaviour
         {
             Vector3 camPos = Camera.main.transform.position;
             RaycastHit2D hitInfo  = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero, 0f);
-            bool res = hitInfo.collider != null;
-            if (res && poses.ContainsKey(hitInfo.collider.name))
+            if (hitInfo.collider != null)
             {
-                GameObject mark = hitInfo.collider.tag == "BrainPart" ? brainMarkGO : bodyMarkGO;
-                mark.transform.position = poses[hitInfo.collider.name];
-                if (hitInfo.collider.tag == "BrainPart")
-                    selectedBrainPart = hitInfo.collider.gameObject;
+                GameObject mark;
+                switch (hitInfo.collider.tag)
+                {
+                    case "BrainPart":
+                        mark = brainMarkGO;
+                        selectedBrainPart = hitInfo.collider.gameObject;
+                        break;
+                    case "BodyPart":
+                        mark = bodyMarkGO;
+                        selectedBodyPart = hitInfo.collider.gameObject;
+                        break;
+                    default:
+                        return;
+                }
+                mark.transform.position = hitInfo.collider.transform.position;
             }
         }
-
-        
     }
 }
