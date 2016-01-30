4f579e4b5d157b1263bdd51dfeddd3be9a4cca23
yuo123 <aaa444777@gmail.com>
2016-01-30 12:59:32 +0200

    Made motor signals a response to sensory signals
    Responses can be set by manipulating the eventLinks field of SignalController in the editor

--- Assets/Scripts/MyUtil.cs
+++ Assets/Scripts/MyUtil.cs
@@ -1,9 +1,4 @@
-﻿
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using UnityEngine;
+﻿using UnityEngine;
 
 public static class MyUtil
 {
@@ -21,6 +16,7 @@ public static class MyUtil
         throw new System.ArgumentException("Invalid Options", "options");
     }
 
+#if DEBUG
     public static SignalType GetRandomSignalType(SignalType[] options, int total, int rand)
     {
         foreach (SignalType type in options)
@@ -32,12 +28,30 @@ public static class MyUtil
         }
         throw new System.ArgumentException("Invalid Options", "options");
     }
+#endif
 
-    public static int GetImportance(this SignalType type)
+    /// <summary>
+    /// Get the enumration number of this type, stored in the left 16 bits of the number
+    /// </summary>
+    public static int GetEnumIndex(this SignalType type)
     {
-        return (int)type & 0x00001111;
+        //clear the right 16 bits of the number, by AND-ing it with 11111111111111110000000000000000 (in the code it is written as hexadecimal)
+        //then, shift it back to the right so it's not multiplied by 256
+        return unchecked((int)(((uint)type & 0xFFFF0000) >> 16));
     }
 
+    /// <summary>
+    /// Get the importance of this signal type, stored in the right 16 bits of the number
+    /// </summary>
+    public static int GetImportance(this SignalType type)
+    {
+        //clear the left 16 bits of the number, by AND-ing it with 00000000000000001111111111111111 (in the code it is written as hexadecimal)
+        return (int)type & 0x0000FFFF;
+    }
+
+    /// <summary>
+    /// Get the weight used for selecting this signal type. This is inversly proportional to its importance
+    /// </summary>
     public static int GetSelectionWeight(this SignalType type)
     {
         return SignalController.MAX_HEALTH - type.GetImportance();
